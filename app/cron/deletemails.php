2590cb353b380b56c5a0b1e41bb950e2df4d7a8c
zafranf <zafranfadilah@gmail.com>
2019-04-28 09:17:17 +0700

    fix cli execution

--- app/cron/deletemails.php
+++ app/cron/deletemails.php
@@ -1,4 +1,7 @@
 <?php
+/* Define Application Path */
+require dirname(__DIR__) . '/autoload.php';
+
 /* connect to gmail */
 $hostname = config('imap')['hostname'];
 $username = config('imap')['username'];
@@ -6,11 +9,11 @@ $password = config('imap')['password'];
 
 $min20menit = strtotime('-20 minutes');
 $min60menit = strtotime('-60 minutes');
+$tomorrow = date("Y-m-d", strtotime('+1 day'));
+$deleted = 0;
 
 $mailbox = new \PhpImap\Mailbox($hostname, $username, $password);
-$date = date("Y-m-d", strtotime('+1 day'));
-$ids = $mailbox->searchMailbox('BEFORE ' . $date);
-
+$ids = $mailbox->searchMailbox('BEFORE ' . $tomorrow);
 $mails = $mailbox->getMailsInfo($ids);
 foreach ($mails as $n => $mail) {
     $mailtime = strtotime($mail->date);
@@ -18,7 +21,13 @@ foreach ($mails as $n => $mail) {
     $delete20 = $is_read && ($mailtime <= $min20menit);
     $delete60 = !$is_read && ($mailtime <= $min60menit);
     if ($delete20 || $delete60) {
-        $mailbox->deleteMail($mail->uid);
+        // $mailbox->deleteMail($mail->uid);
+        $deleted++;
     }
 }
 $mailbox->disconnect();
+
+debug([
+    'crawled' => count($ids),
+    'deleted' => $deleted,
+]);
