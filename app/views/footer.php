ed91f050fce387974b8776979d0ee1256bcddf4c
zafranf <zafranfadilah@gmail.com>
2020-04-21 14:32:18 +0700

    add br, remove gtag

--- app/views/footer.php
+++ app/views/footer.php
@@ -9,15 +9,7 @@
         </small>
 
         <?php if (_server('SERVER_NAME') == "ngetes.com" || _server('SERVER_NAME') == "www.ngetes.com") {?>
-          <!-- Global site tag (gtag.js) - Google Analytics -->
-          <script async src="https://www.googletagmanager.com/gtag/js?id=<?=config('ga_code')?>"></script>
-          <script>
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-
-            gtag('config', '<?=config('ga_code')?>');
-          </script>
+          <br>
           <script type="text/javascript" data-cfasync="false" src="//uprimp.com/bnr.php?section=General&pub=521431&format=728x90&ga=g"></script>
           <noscript><a href="https://yllix.com/publishers/521431" target="_blank"><img src="//ylx-aff.advertica-cdn.com/pub/728x90.png" style="border:none;margin:0;padding:0;vertical-align:baseline;" alt="ylliX - Online Advertising Network" /></a></noscript>
         <?php }?>
