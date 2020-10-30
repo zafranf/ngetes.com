84e898691198978a391abba5dc0dbe5037739075
zafranf <zafranfadilah@gmail.com>
2020-10-31 02:00:28 +0700

    tes

---
+++ public/coba.php
@@ -0,0 +1,15 @@
+<?php
+$logs = [
+    'request' => [
+        'method' => $_SERVER['REQUEST_METHOD'],
+        'uri' => $_SERVER['REQUEST_URI'],
+        'body' => file_get_contents('php://input'),
+        'GET' => $_GET,
+        'POST' => $_POST,
+        'FILES' => $_FILES,
+        'headers' => getallheaders(),
+    ],
+];
+
+$filelog = '../storage/logs/' . date('Ymd') . '.log';
+file_put_contents($filelog, '[' . date("Y-m-d H:i:s") . '] ' . json_encode($logs) . "\n", FILE_APPEND);
