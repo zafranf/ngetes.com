47976f01f9533b4d715b8aa147e72ab844db18bb
zafranf <zafranfadilah@gmail.com>
2020-10-31 01:03:50 +0700

    fix sflag

--- app/controllers/mailgun/hook.php
+++ app/controllers/mailgun/hook.php
@@ -61,7 +61,7 @@ $content = str_replace(['<body>', '</body>'], "", $content);
 
 /* check flag */
 $is_plain = !empty($r['body-plain']) && !empty($content);
-$is_spam = isset($r['X-Mailgun-SFlag']) && bool($r['X-Mailgun-SFlag']) ? 1 : 0;
+$is_spam = isset($r['X-Mailgun-Sflag']) && bool($r['X-Mailgun-Sflag']) ? 1 : 0;
 if (!$is_spam) {
     $is_spam = strpos($r['message-headers'], "\"X-Mailgun-Sflag\", \"yes\"") !== false ? 1 : 0;
 }
