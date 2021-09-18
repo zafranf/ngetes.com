5dde828c0cb19a4d151b3afb6a07615e790403c7
zafranf <zafranfadilah@gmail.com>
2021-09-18 19:11:29 +0700

    exit

--- app/cron/unsubscribe.php
+++ app/cron/unsubscribe.php
@@ -29,75 +29,80 @@ function doUnsub($date = 'now', $page = 1, $urls = [], $mail_ids = [], $last_id 
         ->limit($perpage)
         ->offset($offset)
         ->get();
-    foreach ($mails as $mail) {
-        echo $mail->id . "\n";
+    if (count($mails)) {
+        foreach ($mails as $mail) {
+            echo $mail->id . "\n";
 
-        $cssToInlineStyles = new \TijsVerkoyen\CssToInlineStyles\CssToInlineStyles();
-        $html = $cssToInlineStyles->convert($mail->html);
+            $cssToInlineStyles = new \TijsVerkoyen\CssToInlineStyles\CssToInlineStyles();
+            $html = $cssToInlineStyles->convert($mail->html);
 
-        $doc = new \DOMDocument('1.0', 'UTF-8');
-        @$doc->loadHTML(mb_convert_encoding($html, 'HTML-ENTITIES', 'UTF-8'));
-        $links = $doc->getElementsByTagName('a');
-        foreach ($links as $link) {
-            // debug($link->getAttribute('href'), $link->textContent, $link->nodeValue, $link);
-            // $link->setAttribute('target', '_blank');
-            $match = false;
-            $texts = ['unsubscribe', 'here', 'sini'];
-            foreach ($texts as $text) {
-                if (str_contains(strtolower($link->nodeValue), $text)) {
-                    $match = true;
-                }
-            }
-            if ($match) {
-                $url = $link->getAttribute('href');
-                echo '-> ' . $link->nodeValue . ' : ' . $url . "\n";
-                $unsub = 'Already unsubscribed';
-
-                /* cek url exists */ /* db based on link */
-                $exists = in_array($url, $urls); //db()->table('unsubs')->where('url', $url)->first();
-                if (!$exists) {
-                    try {
-                        /* do unsub */
-                        $client = new \GuzzleHttp\Client();
-                        $res = $client->request('GET', $url, [
-                            'timeout' => 5,
-                            'connect_timeout' => 5
-                        ]);
-                        if ($res->getStatusCode() == 200) {
-                            $unsub = 'SUCCESS';
-
-                            /* input ke db, from to link */
-                            $from = $mail->from_email;
-                            $to = $mail->to;
-                            db()->table('unsubs')->insert([
-                                'from' => $from,
-                                'to' => $to,
-                                'url' => $url,
-                                'email_id' => $mail->id,
-                                'status' => 1,
-                                'updated_at' => date('Y-m-d H:i:s'),
-                                'created_at' => date('Y-m-d H:i:s'),
-                            ]);
-
-                            /* attach to urls */
-                            $urls[] = $url;
-                            $mail_ids[] = $mail->id;
-                        } else {
-                            $unsub = 'Failed!';
-                        }
-                    } catch (\Exception $e) {
-                        $unsub = 'Failed! | ' . $e->getMessage();
+            $doc = new \DOMDocument('1.0', 'UTF-8');
+            @$doc->loadHTML(mb_convert_encoding($html, 'HTML-ENTITIES', 'UTF-8'));
+            $links = $doc->getElementsByTagName('a');
+            foreach ($links as $link) {
+                // debug($link->getAttribute('href'), $link->textContent, $link->nodeValue, $link);
+                // $link->setAttribute('target', '_blank');
+                $match = false;
+                $texts = ['unsubscribe', 'here', 'sini'];
+                foreach ($texts as $text) {
+                    if (str_contains(strtolower($link->nodeValue), $text)) {
+                        $match = true;
                     }
                 }
-                echo "     unsub: " . $unsub . "\n";
-                echo "========================================" . "\n";
+                if ($match) {
+                    $url = $link->getAttribute('href');
+                    echo '-> ' . $link->nodeValue . ' : ' . $url . "\n";
+                    $unsub = 'Already unsubscribed';
+
+                    /* cek url exists */ /* db based on link */
+                    $exists = in_array($url, $urls); //db()->table('unsubs')->where('url', $url)->first();
+                    if (!$exists) {
+                        try {
+                            /* do unsub */
+                            $client = new \GuzzleHttp\Client();
+                            $res = $client->request('GET', $url, [
+                                'timeout' => 5,
+                                'connect_timeout' => 5
+                            ]);
+                            if ($res->getStatusCode() == 200) {
+                                $unsub = 'SUCCESS';
+
+                                /* input ke db, from to link */
+                                $from = $mail->from_email;
+                                $to = $mail->to;
+                                db()->table('unsubs')->insert([
+                                    'from' => $from,
+                                    'to' => $to,
+                                    'url' => $url,
+                                    'email_id' => $mail->id,
+                                    'status' => 1,
+                                    'updated_at' => date('Y-m-d H:i:s'),
+                                    'created_at' => date('Y-m-d H:i:s'),
+                                ]);
+
+                                /* attach to urls */
+                                $urls[] = $url;
+                                $mail_ids[] = $mail->id;
+                            } else {
+                                $unsub = 'Failed!';
+                            }
+                        } catch (\Exception $e) {
+                            $unsub = 'Failed! | ' . $e->getMessage();
+                        }
+                    }
+                    echo "     unsub: " . $unsub . "\n";
+                    echo "========================================" . "\n";
+                }
             }
+
+            $last_id = $mail->id;
         }
 
-        $last_id = $mail->id;
+        doUnsub($date, $page + 1, $urls, $mail_ids, $last_id);
+    } else {
+        echo "FINISH!";
+        exit();
     }
-
-    doUnsub($date, $page + 1, $urls, $mail_ids, $last_id);
 }
 
 
